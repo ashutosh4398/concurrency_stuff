d3b49fc6ec6dd5ea09b5d0775ea1bf7cb2b7dacf
ashutosh4398 <ashutoshdhondkar@gmail.com>
2023-04-22 18:29:19 +0530

    added update count to keep track

--- proj_wiki/companies/models.py
+++ proj_wiki/companies/models.py
@@ -6,6 +6,12 @@ class Companies(models.Model):
     symbol = models.CharField(max_length=64, null=False)
     price = models.FloatField(default=0)
     timestamp = models.DateTimeField(default=datetime.datetime.utcnow)
+    update_count = models.IntegerField(default=0)
 
     def __str__(self) -> str:
         return f"{self.id} -> {self.symbol}"
+    
+    def save(self, *args, **kwargs) -> None:
+        print("Saving")
+        self.update_count += 1
+        return super().save(*args, **kwargs)
