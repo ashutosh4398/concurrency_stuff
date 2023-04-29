0c34fbe8e83b81d34839f9d1c4c11312401079c9
ashutosh4398 <ashutoshdhondkar@gmail.com>
2023-04-29 13:08:45 +0530

    minor change

--- async/yield_from.py
+++ async/yield_from.py
@@ -15,3 +15,36 @@ def basic_example():
         print(i)
 
 # BASIC EXAMPLE ENDS
+
+# ADVANCED EXAMPLE
+def writer():
+    while True:
+        try:
+            w = (yield)
+        except Exception as e:
+            print(e)
+        print(">>>", w)
+
+def writer_wrapper():
+    w = writer()
+    w.send(None)
+    while True:
+        text = (yield)
+        w.send(text)
+
+def writer_wrapper_advance():
+    """
+        yield from does the same work as writer_wrapper()
+    """
+    yield from writer()
+
+wrap = writer_wrapper_advance()
+wrap.send(None) # prime the coroutine
+texts = ["Ashutosh is a software developer", "He has 3 years of experience", "spam"]
+for text in texts:
+    if text == "spam":
+        wrap.throw(Exception("Spam text"))
+    else:
+        wrap.send(text)
+    
+
